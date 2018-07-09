31ddfa5fbae0c0aad38febfd339d618e67592278
Cameron Sanders <csanders@protonmail.com>
2018-07-09 16:13:59 -0500

    add help component

---
+++ src/components/environments/Help.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-navbar :title="'Help'" color="primary" :logo="false" :menu="false"></app-navbar>
+</template>
+
+<script>
+import Navbar from '../organisms/Navbar'
+
+export default {
+  components: {
+    'app-navbar': Navbar
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+</style>
